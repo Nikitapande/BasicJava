74013754e3543259aa04fc380e9dc42718397cf6
Nikita pande <30722238+Nikitapande@users.noreply.github.com>
2024-03-07 15:08:15 +0530

    Add files via upload

---
+++ PackageExmp2.java
@@ -0,0 +1,11 @@
+import mypackages.mypack.packageExmp1;
+// import mypackages.*; --> Exceptional error
+public class PackageExmp2 {
+
+	public static void main(String[] args) {
+		packageExmp1 obj=new packageExmp1();
+		obj.msg();
+
+	}
+
+}
---
+++ PackageExmp3.java
@@ -0,0 +1,9 @@
+import mypackages.mypack.packageExmp1;
+public class PackageExmp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
